2dc0715492befa1e38cce17e4630e62da0943bfc
Jaeho <jsim@tribalscale.com>
2018-05-22 16:47:42 -0400

    account linking temp

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var winston = require('winston');
+const winston = require('winston');
 
 var logger = new (winston.Logger)({
     transports: [
@@ -20,7 +20,7 @@ if(process.env.NODE_DEBUG_EN) {
 exports.handler = (event, context, callback) => {
     try {
 
-        logger.info('event.session.application.applicationId=' + event.session.application.applicationId);
+        logger.info('event.session.application.applicationId=' + event.session. application.applicationId);
 
         if (APP_ID !== '' && event.session.application.applicationId !== APP_ID) {
             context.fail('Invalid Application ID');
@@ -177,7 +177,7 @@ var getError = (err) => {
 //--------------------------------------------- Skill specific logic starts here ----------------------------------------- 
 
 //Add your skill application ID from amazon devloper portal
-var APP_ID = '';
+var APP_ID = 'amzn1.ask.skill.2bc3ed55-95ff-4e45-9f9b-7d15487b6cb9';
 
 var onSessionStarted = (sessionStartedRequest, session) => {
     logger.debug('onSessionStarted requestId=' + sessionStartedRequest.requestId + ', sessionId=' + session.sessionId);
@@ -193,9 +193,8 @@ var onSessionEnded = (sessionEndedRequest, session) => {
 
 var onLaunch = (launchRequest, session, response) => {
   logger.debug('onLaunch requestId=' + launchRequest.requestId + ', sessionId=' + session.sessionId);
-
-  //response.speechText = 'Welcome msg';
-  //response.repromptText = 'Reprompt msg';
+  response.speechText = 'Welcome to email checker skill. You can use this skill to check your gmail unread messages. You can say, whats new?';
+  response.repromptText = 'What do you want to do? whats new to check your unread messages.';
   response.shouldEndSession = false;
   response.done();
 }
@@ -208,3 +207,126 @@ intentHandlers['HelloIntent'] = (request,session,response,slots) => {
   
 }
 **/
+
+intentHandlers['EmailCheckIntent'] = (reqest, session, response, slots) => {
+  getMessages(response, session);
+}
+
+const https = require('https');
+const Promise = require('bluebird');
+const MAX_READ_MESSAGES = 3;
+const MAX_MESSAGES = 20;
+
+var getMessages = (response, session) => {
+  var url;
+  // from gmail api reference - users.messages -> list -> http request
+  url = `https://www.googleapis.com/gmail/v1/users/me/messages?access_token=${session.user.accessToken}&q="is:unread"`;
+  console.log('url: ', url);
+  logger.debug(url);
+  https.get(url, (res) => {
+    var body = '';
+    res.on('data', (chunk) => {
+      var result = JSON.parse(body);
+      var messages;
+      if(result.resultSizeEstimate) {
+        response.speechText = `You have total ${result.resultSizeEstimate} unread mails. `;
+        response.speechText += `Here are your top mails. `;
+        messages = result.messages;
+        if(messages.length > MAX_READ_MESSAGES) {
+          // store in session for 'read more'
+          session.attributes.messages = messages.slice(0, MAX_MESSAGES);
+          messages = result.messages.slice(0, MAX_READ_MESSAGES);
+          session.attributes.offset = MAX_READ_MESSAGES;
+        }
+
+        readMessagesFromIds(messages, response, session);
+      }
+      else {
+        response.fail(body);
+      }
+    });
+  }).on('error', (e) => {
+    response.fail(e);
+  });
+}
+
+var readMessagesFromIds = (messages, response, session) => {
+  logger.debug(messages);
+  var promises = messages.map((messages) => {
+    return new Promise((resolve, reject) => {
+      getMessageFomrId(message.id, session.user.accessToken, (res, err) => {
+        var from = res.payload.headers.find( o => o.name === 'from').value;
+        from = from.replace(/<.*/, '');
+        message.result = {
+          snippet: res.snippet,
+          subject: res.playload.headers.find(o => o.name === 'Subject').value,
+          date: res.playload.headers.find(o => o.name === 'Date').value,
+          from: from
+        };
+        resolve();
+      });
+    });
+  });
+
+  Promise.all(promises).then(() => {
+    messages.forEach((message, idx) => {
+      response.speechText += `<say-as interpret-as="ordinal">${idx+1}</say-as> Mail from ${message.result.from} with subject ${message.result.subject}. `;
+    });
+
+    response.shouldEndSession = true;
+    if(session.attributes.offset && session.attributes.offset > 0) {
+      response.speechText += "Do you want to continue? ";
+      response.reprompText = " You can say yes or stop. ";
+      response.shouldEndSession = false;
+    }
+    response.done();
+  }).catch((err) => {
+    response.fail(err);
+  });
+}
+
+var getMessageFromId = (messageIid, token, callback) => {
+  // gmail api reference - get
+  var url = `https://www.googleapis.com/gmail/v1/users/me/messages/${messageId}?format=metadata&metadataHeaders=subject&metadataHeaders=From&metadataHeaders=Date&access_token=$(token}`;
+  https.get(url, (res) => {
+    var body = '';
+
+    res.on('data', (res) => {
+      body += chunk;
+    });
+
+    res.on('end', (res) => {
+      logger.debug(body);
+      var result = JSON.parse(body);
+      callback(result);
+    });
+  }).on('error', (e) => {
+    logger.error("Got error: ", e);
+    callback('', err);
+  });
+}
+
+
+intentHandlers['AMAZON.YesIntent'] = (reqest, session, response, slots) => {
+  var messages;
+
+  if(session.attributes.messages && session.attributes.offset > 0) {
+    // getting messages from offset because we already got previous ones
+    messages = session.attributes.messages.slice(session.attributes.offset);
+    logger.debug(session.attributes.messages);
+    // if there are more
+    if(messages.length > MAX_READ_MESSAGES) {
+      messages = messages.slice(0, MAX_READ_MESSAGES);
+      session.attributes.offset += MAX_READ_MESSAGES;
+    }
+    else {
+      session.attributes.offset = 0;
+    }
+    readMessagesFromIds(messages, response, session);
+  }
+  else {
+    response.speechText = "Wrong invocation of intent";
+    response.shouldEndSession = true;
+    response.done();
+  }
+}
